67fb3296d40e6b87914f1b3d6153209b01146bad
Ethan Warren <102001092+ethan-warren@users.noreply.github.com>
2024-02-03 01:07:13 -0800

    basic form with textblob
    unglamorized website with basic functionality using flask.

--- program.py
+++ program.py
@@ -2,14 +2,22 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from textblob import TextBlob, Word, Blobber
+from textblob.classifiers import NaiveBayesClassifier
+from textblob.taggers import NLTKTagger
+
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
-def gfg():
+def home():
     if request.method == "POST":
-       # getting input with name = fname in HTML form
-       text = request.form.get("fname")
-       
-       return "Text "+ str(text)
+       text =  request.form["textbox"]
+       sentiment = TextBlob(text).sentiment
+
+       return render_template("index.html", data = {"textInput" : text,
+                                                    "pol" : sentiment.polarity,
+                                                    "subj" : sentiment.subjectivity,})
+    
+    return render_template("index.html", data = {"textInput" : ""})
 
 app.run(debug=True)
