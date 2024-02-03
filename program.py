397894bb169fd716a9e3f20ac06985fc0e6e54b3
Ethan Warren <102001092+ethan-warren@users.noreply.github.com>
2024-02-02 16:34:26 -0800

    Added input form

--- program.py
+++ program.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods = ['GET', 'POST'])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       text = request.form.get("fname")
+       
+       return "Text "+ str(text)
+
+app.run(debug=True)
